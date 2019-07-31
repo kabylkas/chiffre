d2076145521519f88e42d5023541bc26d52ae969
Nursultan Kabylkas <nkabylkas@sc-rtl01.esperanto.ai>
2019-07-31 08:30:46 -0700

    Summary: added Congestors with different parameters
    Congestor.scala - added 9 different configurations for congestor.
    This is to run experiments on how different pulse characteristics
    affect the coverage and performance.

--- src/main/scala/chiffre/inject/Congestor.scala
+++ src/main/scala/chiffre/inject/Congestor.scala
@@ -5,18 +5,19 @@ import chisel3.util._
 
 import chiffre.{ChiffreInjector, InjectorInfo, ScanField}
 
-case class Duration(width: Int) extends ScanField
+case class DurationHigh(width: Int) extends ScanField
+case class DurationLow(width: Int) extends ScanField
 
-case class CongestorInfo(duration: Int) extends InjectorInfo {
-  val fields = Seq(Duration(duration))
+case class CongestorInfo(duration_high: Int, duration_low: Int) extends InjectorInfo {
+  val fields = Seq(DurationHigh(duration_high), DurationLow(duration_low))
 }
 
-class Congestor(bitWidth: Int, duration: Int) extends Injector(1) {
+class Congestor(bitWidth: Int, duration_high: Int, duration_low: Int) extends Injector(1) {
   val sIdle :: sSetCounter :: sCongest :: sSetPassCounter :: sPass :: Nil = Enum(5)
   val state = RegInit(sIdle)
-  val counter = RegInit(0.U(10.W))
+  val counter = RegInit(0.U(16.W))
 
-  lazy val info = CongestorInfo(duration)
+  lazy val info = CongestorInfo(duration_high, duration_low)
 
   val fire = io.scan.en & (state === sCongest)
   io.out := Mux(fire, 1.U, io.in)
@@ -28,7 +29,7 @@ class Congestor(bitWidth: Int, duration: Int) extends Injector(1) {
     }
 
     is(sSetCounter) {
-      counter := duration.U
+      counter := duration_high.U
       state := sCongest
     }
 
@@ -40,7 +41,7 @@ class Congestor(bitWidth: Int, duration: Int) extends Injector(1) {
     }
 
     is (sSetPassCounter) {
-      counter := 511.U
+      counter := duration_low.U
       state := sPass
     }
 
@@ -53,4 +54,18 @@ class Congestor(bitWidth: Int, duration: Int) extends Injector(1) {
   }
 }
 
-class Congestor64(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 64) with ChiffreInjector
+class Congestor64_64(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 64, 64) with ChiffreInjector
+// Period: 1024 cycles
+class Congestor1024_20duty(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 204, 820) with ChiffreInjector
+class Congestor1024_50duty(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 512, 512) with ChiffreInjector
+class Congestor1024_80duty(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 820, 204) with ChiffreInjector
+// Period: 2048 cycles
+class Congestor2048_20duty(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 410, 1638) with ChiffreInjector
+class Congestor2048_50duty(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 1024, 1024) with ChiffreInjector
+class Congestor2048_80duty(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 1638, 410) with ChiffreInjector
+// Period: 4096 cycles
+class Congestor4096_20duty(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 820, 3276) with ChiffreInjector
+class Congestor4096_50duty(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 2048, 2048) with ChiffreInjector
+class Congestor4096_80duty(bitWidth: Int, val scanId: String) extends Congestor(bitWidth, 3276, 820) with ChiffreInjector
+
+
